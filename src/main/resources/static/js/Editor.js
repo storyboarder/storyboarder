cc5171053093a3807c8b7b57380ee2069df9dd8a
Yidan Zeng <yidan_zeng@brown.edu>
2015-03-30 21:36:53 -0400

    editor dependencies

---
+++ src/main/resources/static/js/Editor.js
@@ -0,0 +1,17 @@
+require.config({
+	baseUrl: "../static/js",
+	paths: {
+		jquery: "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js",
+	}
+});
+
+var dependencies = [
+	"Select",
+	"History",
+	"Photo",
+	"Draw",
+	"Text",
+	"Split",
+	"Join",
+
+];
