ddcba55b14e7eccceacc8c7d6171075615fa5064
Yidan Zeng <yidan_zeng@brown.edu>
2015-03-31 19:13:06 -0400

    text tool

--- src/main/resources/static/js/tools/page/Select.js
+++ src/main/resources/static/js/tools/page/Select.js
@@ -9,10 +9,37 @@ define(["../../CanvasState"], function (CanvasState) {
 		var activate = function(CanvasState) {
 			console.log("activate");
 
+
+		var canvas = CanvasState.canvas; 
+		canvas.selection = true; // enable group selection
+
+		// for resizing and snap to grid functionality
+		if(canvas.snapToGrid;) {
+			canvas.on('object:scaling', function(options)) {
+				if(options.e.clientX < distanceToClosestX) {
+					// snap or display snap line
+				}
+
+				if(options.e.clientY < distanceToClosestY) {
+					// snap or display snap ine
+				}
+			};
+		};
+		
+
+		canvas.on('object:selected', function(options)) {
+			if(options.type !== 'text') {
+				var obj = options.target;
+				// obj.onKeyPress(e);
+			}
+		}
+
 		};
 
 		var deactivate = function() {
 			console.log("deactivate");
+
+			canvas.selection = false; // disable group selection
 
 
 		};
