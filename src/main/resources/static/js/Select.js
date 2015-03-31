e713b577d4c24406235c9c472bd7b30d6a097a20
Yidan Zeng <yidan_zeng@brown.edu>
2015-03-30 21:45:16 -0400

    select tool created

---
+++ src/main/resources/static/js/Select.js
@@ -0,0 +1,31 @@
+require.config({
+	baseUrl: "../static/js",
+	paths: {
+		jquery: "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js",
+	}
+});
+
+var dependencies = [
+	"CanvasState"
+];
+
+// Select.js - moving, resizing, deleting canvas items. 
+// It will also take care of changing snap to grid options in the CanvasState.
+
+var Selector = {
+
+	var _helperFunction() {
+	
+	};
+
+	var activate(CanvasState) {
+		// knows the elements (array)
+		// event listener to click + drag?
+
+		
+	};
+
+	var deactivate() {
+
+	};
+};
