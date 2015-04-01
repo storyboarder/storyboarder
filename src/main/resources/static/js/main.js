c4bbd0b51da522c224da21a1fd879bbabf5ef81a
Nellie Robinson <nellie_robinson@brown.edu>
2015-03-31 23:18:52 -0400

    fabric js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,6 +2,7 @@ require.config({
 	baseUrl: "../static/js",
 	paths: {
 		jquery: "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js",
+		fabricjs: "http://fabricjs.com/lib/fabric.js",
 		tools: "tools",
 	}
 });
