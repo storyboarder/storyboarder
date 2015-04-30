833ef54bb36e94bd868d63735192b98fd515f3e7
Yidan Zeng <yidan_zeng@brown.edu>
2015-04-29 20:32:36 -0400

    Fill

---
+++ src/main/resources/static/js/tools/image/Fill.js
@@ -0,0 +1,36 @@
+define(["../../CanvasState"], function (canvasState) {
+
+		var activate = function() {
+      var selected;
+      canvas.on("mouse:down", function(coor) {
+        selected = coor.target;
+
+        if(selected && selected.elmType === "panel") {
+          console.log(selected);
+          console.log("YAY!");
+          selected["fill"] = $("#fill-color").val();
+          canvas.renderAll();
+        }
+
+      });
+
+      return this;
+
+		};
+
+		var deactivate = function() {
+      canvas.__eventListeners["mouse:down"] = [];
+		};
+
+	
+	return {
+		name: "Fill",
+		init: function () {
+			console.log("init fill");
+			canvas = canvasState.getCanvas();
+		},
+		activate: activate,
+		deactivate: deactivate
+	};
+	
+});
