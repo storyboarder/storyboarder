39cab2e70e856afe9e0f72dc4d664e8e6c9b3352
Filip <filip_bystricky@brown.edu>
2015-05-02 22:13:39 -0400

    0 checkstyle, and 0 findbugs!

--- src/main/java/sqlutil/package-info.java
+++ src/main/java/sqlutil/package-info.java
@@ -1,13 +1,13 @@
 /**
  * Contains classes to run a Storyboarder project:
  * <ul>
- * <li> {@link sqlutil.ResultConverter} is an interface for converting one row of
- * a SQL ResultSet to the object stored in that row.</li>
+ * <li> {@link sqlutil.ResultConverter}, an interface for converting one row of
+ * an SQL ResultSet to the object stored in that row.</li>
  * <li>  {@link sqlutil.ResultConverters} is a static class that stores basic
  * ResultConverters.</li>
  * <li>  {@link sqlutil.SqlQueryer} handles queries to an SQL database.</li>
- * <li>  {@link sqlutil.SqlString} represents an SQL query and helps safely build
- * parametric queries with many parameters.</li>
+ * <li>  {@link sqlutil.SqlString} represents an SQL query and helps safely
+ * construct parametric queries with many parameters.</li>
  * </ul>
  *
  * @author narobins
